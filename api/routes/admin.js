e79fb0c4900720c73b2efa7d662a248090308e86
unknown <opokudavid446@gmail.com>
2020-06-11 18:55:37 +0100

    Changes to client and admin routes

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,9 +1,51 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.status(200).json({
     message: "Post request through",
+  });
+});
+
+router.post("/products", (req, res, next) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+
+  res.status(200).json({ status: 200, product });
+});
+
+router.get("/products/:id", (req, res, next) => {
+  const id = req.params.id;
+  res.status(200).json({
+    message: `Products GET request for ${id}`,
+  });
+});
+
+router.patch("/products/:id", (req, res, next) => {
+  const id = req.params.id;
+  res.status(200).json({
+    message: `Admin Products PATCH request for ${id}`,
+  });
+});
+
+router.delete("/products:id", (req, res, next) => {
+  const id = req.params.id;
+  res.status(200).json({
+    message: `Admin Products DELETE request for ${id}`,
+  });
+});
+
+router.get("/orders", (req, res, next) => {
+  res.status(200).json({
+    message: "Get request to admin orders",
+  });
+});
+
+router.post("/orders", (req, res, next) => {
+  res.status(200).json({
+    message: "post request to admin orders",
   });
 });
 
--- api/routes/products.js
+++ api/routes/products.js
@@ -8,33 +8,10 @@ router.get("/", (req, res, next) => {
   });
 });
 
-router.post("/", (req, res, next) => {
-  const product = {
-    name: req.body.name,
-    price: req.body.price,
-  };
-
-  res.status(200).json({ status: 200, product });
-});
-
 router.get("/:id", (req, res, next) => {
   const id = req.params.id;
   res.status(200).json({
     message: `Products GET request for ${id}`,
-  });
-});
-
-router.patch("/:id", (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).json({
-    message: `Products PATCH request for ${id}`,
-  });
-});
-
-router.delete("/:id", (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).json({
-    message: `Products DELETE request for ${id}`,
   });
 });
 
